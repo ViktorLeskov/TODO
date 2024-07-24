9d9ec29b343957e37e46804c3f4e774a7ca355f0
ViktorLeskov <viktor.leskov@protonmail.com>
2024-07-24 19:23:33 +0200

    'task' and 'request' state database model added

--- composeApp/src/commonMain/kotlin/domain/ToDoTask.kt
+++ composeApp/src/commonMain/kotlin/domain/ToDoTask.kt
@@ -1,1 +1,14 @@
 package domain
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+class ToDoTask : RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+    var title: String = ""
+    var description: String = ""
+    var favourite: Boolean = false
+    var completed: Boolean = false
+}
