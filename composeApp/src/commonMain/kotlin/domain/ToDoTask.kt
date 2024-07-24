0be510ee8311bea9487e6c92d75d54e857dde32a
ViktorLeskov <viktor.leskov@protonmail.com>
2024-07-24 20:01:34 +0200

    TaskScreen updated

--- composeApp/src/commonMain/kotlin/domain/ToDoTask.kt
+++ composeApp/src/commonMain/kotlin/domain/ToDoTask.kt
@@ -4,11 +4,11 @@ import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 import org.mongodb.kbson.ObjectId
 
-class ToDoTask : RealmObject {
+class ToDoTask: RealmObject {
     @PrimaryKey
     var _id: ObjectId = ObjectId()
     var title: String = ""
     var description: String = ""
-    var favourite: Boolean = false
+    var favorite: Boolean = false
     var completed: Boolean = false
 }
